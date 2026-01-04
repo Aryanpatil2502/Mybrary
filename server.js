4cf133d1dd64cf6fcf29de9401352f6e3fca84d3
Aryan Patil <aryanpatil3405@gmail.com>
2026-01-04 17:45:14 +0530

    Initial Author Changes

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 require('dotenv').config()
+console.log("DB:", process.env.DATABASE_URL)
+
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
 
+
+/* const bodyParser = require('body-parser') */
+
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 
 mongoose.connect(process.env.DATABASE_URL)
 
@@ -20,10 +27,11 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
 
-
+ 
 
 
 
